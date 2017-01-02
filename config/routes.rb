c62d2951cfc0a795b86e41e0814fe8ed30f91733
egglestn <segglestn@gmail.com>
2017-01-02 12:39:48 +0000

    Remove admins, add profile fields to users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users
+  # devise_for :users, controllers: { registrations: "registrations" }
+
   resources :programmes
   resources :exercises
+  resources :users
+
+  get "/", to: "programmes#index"
+  root to: "programmes#index"
 end
