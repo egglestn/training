a0cfc76a17c473877b070b24d6bf54b91937378e
egglestn <segglestn@gmail.com>
2017-01-23 19:45:55 +0000

    Change from SQLite to PG

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170111190618) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "exercises", force: :cascade do |t|
     t.string   "name"
     t.string   "link"
@@ -22,7 +25,7 @@ ActiveRecord::Schema.define(version: 20170111190618) do
     t.integer  "programme_id"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
-    t.index ["programme_id"], name: "index_exercises_on_programme_id"
+    t.index ["programme_id"], name: "index_exercises_on_programme_id", using: :btree
   end
 
   create_table "programmes", force: :cascade do |t|
@@ -31,7 +34,7 @@ ActiveRecord::Schema.define(version: 20170111190618) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "customer_id"
-    t.index ["user_id"], name: "index_programmes_on_user_id"
+    t.index ["user_id"], name: "index_programmes_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,8 +53,10 @@ ActiveRecord::Schema.define(version: 20170111190618) do
     t.string   "first_name"
     t.string   "last_name"
     t.integer  "role",                   default: 0
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "exercises", "programmes"
+  add_foreign_key "programmes", "users"
 end
